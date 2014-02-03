6e72ea93f0c7ebfcadd9822679e22a2fa8fe7808
James Zhang <jzhangx2@gmail.com>
2014-02-03 13:21:05 -0500

    Homework 1

---
+++ Assets/Rando_Script.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rando_Script : MonoBehaviour {
+
+	public string message = "Bonjour Monde!";
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello, World!");
+		GetComponent<TextMesh> ().text = message;
+		// Display hello world in French slang in our 3D Text
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
